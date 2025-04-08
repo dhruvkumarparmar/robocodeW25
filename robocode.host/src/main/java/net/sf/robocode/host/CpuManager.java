fc1570aaf0b7975003f3cae1f9676995c2619702
sakkii0910 <muliksakshi906@gmail.com>
2025-04-07 20:05:07 -0400

    Security Hotspot Software Vulnerability reported by SonarCloud

--- robocode.host/src/main/java/net/sf/robocode/host/CpuManager.java
+++ robocode.host/src/main/java/net/sf/robocode/host/CpuManager.java
@@ -59,7 +59,7 @@ public class CpuManager implements ICpuManager { // NO_UCD (use default)
 		setStatus("");
 	}
 
-	@SuppressWarnings("java:S2245") // Not security-sensitive: Used only for CPU performance testing
+	@SuppressWarnings("java:S2245") // Not security-sensitive: CPU benchmarking only
 	private void setCpuConstant() {
 		long count = 0;
 		double d = 0;
